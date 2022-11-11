4acf527562a14461d2f04d577704650e4ee98e75
Favour Ojo <101338048+Lloyd-the-god@users.noreply.github.com>
2022-11-11 13:55:54 +0100

    Update even.js

--- Csc Project/even.js
+++ Csc Project/even.js
@@ -2,12 +2,15 @@ const inputSta = document.getElementById("input-sta");
 const inputEnd = document.getElementById("input-end");
 const btnEl = document.querySelector("#btn");
 let sum = 0;
+let arr = [];
 
 btnEl.addEventListener('click', function(){
-      for(let i = inputSta.value; i <= inputEnd.value; i++){
+      for(let i = parseInt(inputSta.value); i <= parseInt(inputEnd.value); i++){
           if(i % 2 == 0){
+              arr.push(i);
               sum = sum + i;
             }
         }
+        document.querySelector("#length").textContent = "The number of even numbers in this range is "+ arr.length
         document.querySelector('#answer').textContent =  'The sum of even numbers from '+ inputSta.value + '-' + inputEnd.value + ' is ' + sum;
     });
